6e55c9e3822b6b67b05d02f1a1afc4b9d6e46164
Greg Miller <hypurban86@gmail.com>
2026-04-01 01:28:09 -0400

    Adjust to build demo and lib

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,48 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const isLibrary = process.env.BUILD_MODE === 'lib'
+
+  const config = {
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
+  }
+
+  if (isLibrary) {
+    config.build = {
+      lib: {
+        entry: resolve(__dirname, 'src/pnp-lib/index.js'),
+        name: 'VuePickNPlop',
+        fileName: (format) => `vue-pick-n-plop.${format}.js`,
+      },
+      rollupOptions: {
+        external: ['vue', 'gdraghelper'],
+        output: {
+          globals: {
+            vue: 'Vue',
+            gdraghelper: 'DragHelper'
+          },
+        },
+      },
+    }
+  } else {
+    // Standard app build for the demos
+    config.build = {
+      outDir: 'dist-demo'
+    }
+  }
+
+  return config
 })
